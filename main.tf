0a1e0bc770add74d6b817df25887ad036a1aad67
Luiz Mandico <luizmandico@gmail.com>
2024-05-13 22:23:37 -0300

    Add Azure storage account and container resources to main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "azr-rg-demo"
   location = "eastus"
 }
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "azrstgdemo"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "banana"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
