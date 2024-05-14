9263b5fcaa639fe6feb86fdecc95ce5cc3102eb0
Luiz Mandico <luizmandico@gmail.com>
2024-05-13 22:19:42 -0300

    Update Azure PostgreSQL server configuration in main.tf

--- main.tf
+++ main.tf
@@ -15,23 +15,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "azr-rg-demo"
   location = "eastus"
 }
-
-resource "azurerm_postgresql_server" "postgresql" {
-  name                = "azr-postgresql-server"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "B_Gen5_1"
-  storage_mb          = 5120
-  version             = "11"
-  administrator_login = "mandico"
-  administrator_login_password = "P@ssw0rd123!"
-  ssl_enforcement_enabled = true
-}
-
-resource "azurerm_postgresql_database" "database" {
-  name                = "azr-postgresql-db"
-  server_name         = azurerm_postgresql_server.postgresql.name
-  resource_group_name = azurerm_resource_group.rg.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
-}
