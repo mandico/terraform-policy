33fe56f5f0d224358fd4f4ac65266f9f7f34c537
Luiz Mandico <luizmandico@gmail.com>
2024-05-14 21:34:56 -0300

    Add Azure MySQL server resource to main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,22 @@ resource "azurerm_resource_group" "rg" {
   name     = "azr-rg-demo"
   location = "eastus"
 }
+
+resource "azurerm_mysql_server" "mysql" {
+  name                = "my-mysql-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "B_Gen5_1"
+  storage_mb          = 5120
+  version             = "5.7"
+
+  administrator_login          = "adminuser"
+  administrator_login_password = "P@ssw0rd1234"
+
+  ssl_enforcement_enabled = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+
+  tags = {
+    environment = "dev"
+  }
+}
