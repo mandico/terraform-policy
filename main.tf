d8a60a4d0f46227ebfeef8c972d5786179889689
Luiz Mandico <luizmandico@gmail.com>
2024-05-14 21:38:27 -0300

    Refactor Azure MySQL server resource to Azure App Configuration resource in main.tf

--- main.tf
+++ main.tf
@@ -15,19 +15,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "azr-rg-demo"
   location = "eastus"
 }
-
-resource "azurerm_mysql_flexible_server" "mysql_server" {
-  name                = "my-flexible-server"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku_name            = "Standard_D2s_v3"
-  storage_profile {
-    storage_mb = 5120
-  }
-  administrator_login          = "adminuser"
-  administrator_login_password = "P@ssw0rd123!"
-  version                      = "8.0"
-  backup_retention_days        = 7
-  auto_grow_enabled            = true
-  ssl_enforcement_enabled      = true
+resource "azurerm_app_configuration" "appconf" {
+  name                = "azr-app-config-demo"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 }
